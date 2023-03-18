2fb62532bb00011d5a903c556196995a08b1dd2d
AhmedTx <63393122+AhmedTxl@users.noreply.github.com>
2023-03-19 02:00:12 +0300

    Initiate Repo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -9,18 +5,20 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
 
+<template>
+  <HelloWorld />
+</template>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,107 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <template v-if="activePhase == 1">
+    <main class="containerDiv">
+      <!-- <h1>{{ msg }}</h1> -->
+      <div class="bg-[#2f394b9e] circularBackground">
+        <svg class="" width="17" height="16" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z"
+            fill="#FC7614" />
+        </svg>
+      </div>
+      <h1 class="text-3xl font-medium mt-10">How did we do?</h1>
+      <p class="text-[15px] leading-6 text-gray-400 mt-4">
+        {{ paragraph }}
+      </p>
+      <ul id="ratingsList" class="flex flex-row justify-around mt-4">
+        <li @click="checkSelectedChoice" :id="'choice' + label"
+          class="flex flex-row circularBackground bg-[#2f394b9e] hover:bg-orange-600 active:bg-orange-700"
+          v-for="label in ratings" :key="label">
+          <input v-model="selected" :value="label" type="radio" name="ratings" :id="'choice' + label">
+          <span class="select-none mt-1" :for="'rating' + label">{{ label }}</span>
+        </li>
+      </ul>
+      <p class="mt-4">Number {{ ratingChoice }} is choosen</p>
+      <button @click.prevent="activePhase = 2"
+        class="bg-orange-600 uppercase tracking-wider hover:bg-slate-100 active:bg-slate-200 text-white-700 font-normal hover:text-orange-600 py-3 px-4 rounded-3xl mt-6">
+        submit
+      </button>
+    </main>
+  </template>
+
+  <Transition name="fade">
+  <template v-if="activePhase == 2">
+    <main class="containerDiv max-w-xs" style="max-width: 410px;">
+      <!-- <h1>{{ msg }}</h1> -->
+      <div class="self-center mt-2">
+        <img src="../assets/illustration-thank-you.svg" alt="">
+      </div>
+      <div class="p-1 pr-4 pl-4 rounded-2xl self-center text-orange-400 text-[15px] mt-8" 
+           style="background: rgb(34 43 54);">
+        You selected {{ ratingChoice }} out of 5
+      </div>
+      <h1 class="self-center mt-9 text-3xl font-semibold">Thank you!</h1>
+      <p class="self-center text-center text-[15px] text-gray-400 mt-5">
+        We appreciate you taking the time to give a rating. If
+        you ever need more support, don’t hesitate to get in touch!
+      </p>
+    </main>
+  </template>
+</Transition>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script setup>
+import { ref } from "vue";
+
+var ratingChoice = 1;
+
+function checkSelectedChoice() {
+
+  var ul = document.getElementById("ratingsList");
+  var items = ul.getElementsByTagName("li");
+
+  for (var i = 0; i < items.length; ++i) {
+    if (items[i].firstChild.checked) {
+      ratingChoice = items[i].firstChild.value;
+
+      document.getElementById(items[i].firstChild.id).classList.remove('bg-[#2f394b9e]');
+      document.getElementById(items[i].firstChild.id).classList.add('bg-[#3e495d]');
+    } else {
+      document.getElementById(items[i].firstChild.id).classList.remove('bg-[#3e495d]');
+      document.getElementById(items[i].firstChild.id).classList.add('bg-[#2f394b9e]');
+    }
+  }
+}
+
+var activePhase = ref(1);
+const selected = ref(1);
+const paragraph = ref(
+  "Please let us know how we did with your support request." +
+  "All feedback is appreciated to help us improve our offering!"
+);
+const ratings = ["1", "2", "3", "4", "5"];
+</script>
+
+<!--Add "scoped" attribute to limit CSS to this component only-->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@import "../main.css";
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
